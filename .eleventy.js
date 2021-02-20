9b9f21fe363e565546165d1aa85fea9d9a0d4169
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-02-20 18:28:25 +0100

    more updates

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
+const Image = require("@11ty/eleventy-img");
+const sharp = require("sharp");
+const embedInstagram = require("eleventy-plugin-embed-instagram");
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
@@ -19,6 +22,9 @@ module.exports = function (eleventyConfig) {
 
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  // Instagram stuff
+  eleventyConfig.addPlugin(embedInstagram);
 
   // To Support .yaml Extension in _data
   // You may remove this if you can use JSON
@@ -44,6 +50,58 @@ module.exports = function (eleventyConfig) {
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
+  eleventyConfig.addNunjucksAsyncShortcode(
+    "Image",
+    async (src, alt, classImage, classContainer) => {
+      if (!alt) {
+        throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+      }
+
+      let stats = await Image(src, {
+        widths: [25, 300, 600, 900, 1200, 2400],
+        formats: ["jpg", "webp"],
+        urlPath: "/static/img/",
+        outputDir: "./_site/images/",
+      });
+
+      let lowestSrc = stats["jpg"][0];
+
+      const placeholder = await sharp(lowestSrc.outputPath)
+        .resize({ fit: sharp.fit.inside })
+        .blur()
+        .toBuffer();
+
+      const base64Placeholder = `data:image/png;base64,${placeholder.toString(
+        "base64"
+      )}`;
+
+      const srcset = Object.keys(stats).reduce(
+        (acc, format) => ({
+          ...acc,
+          [format]: stats[format].reduce(
+            (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+            ""
+          ),
+        }),
+        {}
+      );
+
+      const source = `<source type="image/webp" data-srcset="${srcset["webp"]}" >`;
+
+      const img = `<img
+      class="lazy ${classImage}"
+      alt="${alt}"
+      src="${base64Placeholder}"
+      data-src="${lowestSrc.url}"
+      data-sizes='(min-width: 1024px) 1024px, 100vw'
+      data-srcset="${srcset["jpeg"]}"
+      width="${lowestSrc.width}"
+      height="${lowestSrc.height}">`;
+
+      return `<picture class="${classContainer}">${source} ${img}</picture>`;
+    }
+  );
+
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     // Eleventy 1.0+: use this.inputPath and this.outputPath instead
@@ -51,7 +109,7 @@ module.exports = function (eleventyConfig) {
       let minified = htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
+        collapseWhitespace: true,
       });
       return minified;
     }
