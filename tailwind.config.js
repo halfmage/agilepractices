badb0e93a138810f1dc6d4403973a02ae8413a61
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-02-24 22:55:25 +0100

    typography fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,26 @@ module.exports = {
         primary: colors.amber,
         secondary: colors.cyan,
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            h1: { color: theme("colors.gray.100") },
+            h2: { color: theme("colors.gray.100") },
+            h3: { color: theme("colors.gray.100") },
+            h4: { color: theme("colors.gray.100") },
+            a: { color: theme("colors.primary.400") },
+            strong: { color: theme("colors.gray.100") },
+            hr: { borderColor: theme("colors.gray.800") },
+            blockquote: {
+              color: theme("colors.gray.200"),
+              borderLeftColor: theme("colors.primary.800"),
+            },
+          },
+        },
+      }),
+      theme: {
+        extend: {},
+      },
     },
   },
   variants: {},
