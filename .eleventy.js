50ad1de2cd8b6ecbf0bb8b2803e67c14cc2849d9
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2021-03-07 16:49:45 +0100

    config and package updates

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,18 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
-const Image = require("@11ty/eleventy-img");
-const sharp = require("sharp");
 const embedInstagram = require("eleventy-plugin-embed-instagram");
 
 module.exports = function (eleventyConfig) {
+  // Copy Static Files to /_Site
+  eleventyConfig.addPassthroughCopy({
+    "./_tmp/static/css/style.css": "./static/css/style.css",
+    "./src/admin/config.yml": "./admin/config.yml",
+    "./node_modules/alpinejs/dist/alpine.js": "./static/js/alpine.js",
+    "./node_modules/prismjs/themes/prism-tomorrow.css":
+      "./static/css/prism-tomorrow.css",
+  });
+
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
 
@@ -20,9 +27,6 @@ module.exports = function (eleventyConfig) {
     );
   });
 
-  // Syntax Highlighting for Code blocks
-  eleventyConfig.addPlugin(syntaxHighlight);
-
   // Instagram stuff
   eleventyConfig.addPlugin(embedInstagram);
 
@@ -34,73 +38,6 @@ module.exports = function (eleventyConfig) {
 
   // Add Tailwind Output CSS as Watch Target
   eleventyConfig.addWatchTarget("./_tmp/static/css/style.css");
-
-  // Copy Static Files to /_Site
-  eleventyConfig.addPassthroughCopy({
-    "./_tmp/static/css/style.css": "./static/css/style.css",
-    "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/alpine.js": "./static/js/alpine.js",
-    "./node_modules/prismjs/themes/prism-tomorrow.css":
-      "./static/css/prism-tomorrow.css",
-  });
-
-  // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("./src/static/img");
-
-  // Copy favicon to route of /_site
-  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
-
-  eleventyConfig.addNunjucksAsyncShortcode(
-    "Image",
-    async (src, alt, classImage, classContainer) => {
-      if (!alt) {
-        throw new Error(`Missing \`alt\` on myImage from: ${src}`);
-      }
-
-      let stats = await Image(src, {
-        widths: [25, 300, 600, 900, 1200, 2400],
-        formats: ["jpg", "webp"],
-        urlPath: "/static/img/",
-        outputDir: "./_site/images/",
-      });
-
-      let lowestSrc = stats["jpg"][0];
-
-      const placeholder = await sharp(lowestSrc.outputPath)
-        .resize({ fit: sharp.fit.inside })
-        .blur()
-        .toBuffer();
-
-      const base64Placeholder = `data:image/png;base64,${placeholder.toString(
-        "base64"
-      )}`;
-
-      const srcset = Object.keys(stats).reduce(
-        (acc, format) => ({
-          ...acc,
-          [format]: stats[format].reduce(
-            (_acc, curr) => `${_acc} ${curr.srcset} ,`,
-            ""
-          ),
-        }),
-        {}
-      );
-
-      const source = `<source type="image/webp" data-srcset="${srcset["webp"]}" >`;
-
-      const img = `<img
-      class="lazy ${classImage}"
-      alt="${alt}"
-      src="${base64Placeholder}"
-      data-src="${lowestSrc.url}"
-      data-sizes='(min-width: 1024px) 1024px, 100vw'
-      data-srcset="${srcset["jpeg"]}"
-      width="${lowestSrc.width}"
-      height="${lowestSrc.height}">`;
-
-      return `<picture class="${classContainer}">${source} ${img}</picture>`;
-    }
-  );
 
   // Minify HTML
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
